18b46cb755615785595a10a2ac3256b5c4238bcd
Johannes Hartel <info@jojojux.de>
2023-08-26 23:28:20 +0000

    Added commands to webdev - Added command caniuse - Added command regex101 - Added alias regx to regex101

--- module/webdev.js
+++ module/webdev.js
@@ -13,3 +13,19 @@ webdev.registerCommand("keycode", "Open keycode explorer", async (full, rest) =>
     window.modifyHref(`https://keycode.info/`);
     out("Please wait...");
 })
+
+webdev.registerCommand("caniuse", "Start a 'Can I use' search", async (full, rest) => {
+    if (rest) {
+        window.modifyHref(`https://caniuse.com/?search=${encodeURIComponent(rest)}`);
+    } else {
+        window.modifyHref("https://caniuse.com/")
+    }
+    out("Please wait...");
+})
+
+webdev.registerCommand("regex101", "Open regex101", async (full, rest) => {
+    window.modifyHref(`https://regex101.com/`);
+    out("Please wait...");
+})
+
+webdev.registerCommand("regx", "Alias: regex101", alias("regex101"))
