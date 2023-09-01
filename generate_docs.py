6930642dca1fa19e393bf83631448288d1fbb71b
JoJoJux <80523427+J0J0HA@users.noreply.github.com>
2023-09-02 00:03:51 +0200

    Small fixes

--- generate_docs.py
+++ generate_docs.py
@@ -9,15 +9,14 @@ with open("GeneratedDocs.md", "w", encoding="UTF-8") as f:
     f.write(f"[Add this repo](https://gterminal.is-a.dev/?cmd=modules+add-repo+{quote(repo['address'])})\n\n")
     for id, module in repo["modules"].items():
         f.write(f"## [`{id}`] {module['name']}\n\n")
+        f.write(f"[Install this module](https://gterminal.is-a.dev/?cmd=modules+install+{repo['id']}:{id})\n\n")
         f.write(f"{module['description']}\n\n")
         f.write(f"Script: [`{module['script']}`]({module['script']})\n\n")
-        f.write(f"{module['description']}\n\n")
-        f.write(f"[Install this module](https://gterminal.is-a.dev/?cmd=modules+install+{repo['id']}:{id})\n\n")
         f.write("Commands:  \n\n")
         for cmd, command in module["commands"].items():
             f.write(f"- `{cmd}`  \n")
-            f.write(f"  {command['description']}  \n")
             f.write(f"[Run this command](https://gterminal.is-a.dev/?cmd={quote(cmd)})\n\n")
+            f.write(f"  {command['description']}  \n")
             if "aliases" in command:
                 f.write("\n  Aliases:  \n\n")
                 for alias in command["aliases"]:
