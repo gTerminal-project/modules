c1c80fba50a003608e0e5dff1ca10a52cdc3831b
JoJoJux <80523427+J0J0HA@users.noreply.github.com>
2023-09-02 00:08:37 +0200

    Small fixes

--- generate_docs.py
+++ generate_docs.py
@@ -6,16 +6,16 @@ with open("repo.json", "r", encoding="UTF-8") as f:
     
 with open("GeneratedDocs.md", "w", encoding="UTF-8") as f:
     f.write(f"# [`{repo['id']}`] {repo['name']}\n\n")
-    f.write(f"[Add this repo](https://gterminal.is-a.dev/?cmd=modules+add-repo+{quote(repo['address'])})\n\n")
+    f.write(f"[![Add repo `{repo['id']}`](https://img.shields.io/badge/Add_repo-{repo['id']}-blue)](https://gterminal.is-a.dev/?cmd=modules+add-repo+{quote(repo['address'])})\n\n")
     for id, module in repo["modules"].items():
         f.write(f"## [`{id}`] {module['name']}\n\n")
-        f.write(f"[Install this module](https://gterminal.is-a.dev/?cmd=modules+install+{repo['id']}:{id})\n\n")
+        f.write(f"[![Install module `{repo['id']}:{id}`](https://img.shields.io/badge/Install_module-{repo['id']}:{id}-blue)](https://gterminal.is-a.dev/?cmd=modules+install+{repo['id']}:{id})\n\n")
         f.write(f"{module['description']}\n\n")
         f.write(f"Script: [`{module['script']}`]({module['script']})\n\n")
         f.write("Commands:  \n\n")
         for cmd, command in module["commands"].items():
             f.write(f"- `{cmd}`  \n")
-            f.write(f"[Run this command](https://gterminal.is-a.dev/?cmd={quote(cmd)})\n\n")
+            f.write(f"[![Run command `{cmd}`](https://img.shields.io/badge/Run_command-{cmd}:{id}-green)](https://gterminal.is-a.dev/?cmd={quote(cmd)})\n\n")
             f.write(f"  {command['description']}  \n")
             if "aliases" in command:
                 f.write("\n  Aliases:  \n\n")
