790800297fd5ec2f021affb25db87794b00d0713
Restyled.io <commits@restyled.io>
2023-10-01 18:54:13 +0000

    Restyled by clang-format

--- module/github.js
+++ module/github.js
@@ -1,45 +1,52 @@
-const github = gterminal.modules.register(
-    "main:github"
-)
+const github = gterminal.modules.register("main:github")
 
 github.registerCommand("github", async (full, rest) => {
-    if (rest) {
-        const srest = rest.split("/");
-        if (srest[1]) {
-            gterminal.web.goto(`https://github.com/${encodeURIComponent(srest[0] || "J0J0HA")}/${encodeURIComponent(srest[1])}`);
-        } else {
-            gterminal.web.goto(`https://github.com/${encodeURIComponent(srest[0] || "J0J0HA")}`);
-        }
+  if (rest) {
+    const srest = rest.split("/");
+    if (srest[1]) {
+      gterminal.web.goto(
+          `https://github.com/${encodeURIComponent(srest[0] || "J0J0HA")}/${
+              encodeURIComponent(srest[1])}`);
     } else {
-        gterminal.web.goto("https://github.com/")
+      gterminal.web.goto(
+          `https://github.com/${encodeURIComponent(srest[0] || "J0J0HA")}`);
     }
-    gterminal.io.println("Please wait...");
+  } else {
+    gterminal.web.goto("https://github.com/")
+  }
+  gterminal.io.println("Please wait...");
 })
 
 github.registerCommand("gist", async (full, rest) => {
-    if (rest) {
-        const srest = rest.split("/");
-        if (srest[1]) {
-            gterminal.web.goto(`https://gist.github.com/${encodeURIComponent(srest[0] || "J0J0HA")}/${encodeURIComponent(srest[1])}`);
-        } else {
-            gterminal.web.goto(`https://github.com/${encodeURIComponent(srest[0] || "J0J0HA")}`);
-        }
+  if (rest) {
+    const srest = rest.split("/");
+    if (srest[1]) {
+      gterminal.web.goto(`https://gist.github.com/${
+          encodeURIComponent(srest[0] ||
+                             "J0J0HA")}/${encodeURIComponent(srest[1])}`);
     } else {
-        gterminal.web.goto("https://gist.github.com/")
+      gterminal.web.goto(
+          `https://github.com/${encodeURIComponent(srest[0] || "J0J0HA")}`);
     }
-    gterminal.io.println("Please wait...");
+  } else {
+    gterminal.web.goto("https://gist.github.com/")
+  }
+  gterminal.io.println("Please wait...");
 })
 
 github.registerCommand("github-pages", async (full, rest) => {
-    if (rest) {
-        const srest = rest.split("/");
-        if (srest[1]) {
-            gterminal.web.goto(`https://${encodeURIComponent(srest[0] || "J0J0HA")}.github.io/${encodeURIComponent(srest[1])}`);
-        } else {
-            gterminal.web.goto(`https://${encodeURIComponent(srest[0] || "J0J0HA")}.github.io/`);
-        }
+  if (rest) {
+    const srest = rest.split("/");
+    if (srest[1]) {
+      gterminal.web.goto(
+          `https://${encodeURIComponent(srest[0] || "J0J0HA")}.github.io/${
+              encodeURIComponent(srest[1])}`);
     } else {
-        gterminal.web.goto("https://github.io/")
+      gterminal.web.goto(
+          `https://${encodeURIComponent(srest[0] || "J0J0HA")}.github.io/`);
     }
-    gterminal.io.println("Please wait...");
+  } else {
+    gterminal.web.goto("https://github.io/")
+  }
+  gterminal.io.println("Please wait...");
 })
